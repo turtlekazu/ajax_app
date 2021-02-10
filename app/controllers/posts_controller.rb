9241d034ad1542c6478f8ff4f3e66262ad4e55b1
turtlekazu <turtlekazu@icloud.com>
2021-02-10 14:46:00 +0900

    既読機能のサーバー実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,21 @@ class PostsController < ApplicationController
   end
 
   def create
-    binding.pry
     post = Post.new(content: params[:content])
     post.save
     # Post.create(content: params[:content])
     redirect_to action: :index
+  end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: {post: item}
   end
 end
